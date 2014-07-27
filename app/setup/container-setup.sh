16f036b99d1624daf4eac33974856a4c1bb56a6b
datfinesoul <philip.hadviger@phad.me>
2014-07-26 19:05:11 -0700

    try to throw a more visible error when container-create failed

--- app/setup/container-setup.sh
+++ app/setup/container-setup.sh
@@ -13,3 +13,5 @@ echo -e "\n127.0.0.1    $(< /etc/hostname)" >> /etc/hosts
 echo -e "\nubuntu ALL=NOPASSWD:ALL" >> /etc/sudoers
 apt-get -y update
 apt-get -y install --force-yes $(< "/var/starphleet/share/lxc-base.packages")
+
+touch  /var/starphleet/share/setup.completed
